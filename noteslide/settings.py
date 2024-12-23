72493f6a3b63247dc5bdf4760616665fad469154
Aditya Byju <byjuaditya@gmail.com>
2024-12-23 10:54:10 -0600

    remove dev params

--- noteslide/settings.py
+++ noteslide/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['noteslidebackend.onrender.com', '127.0.0.1', '28b9-104-176-67-99.ngrok-free.app']
+# '127.0.0.1', '28b9-104-176-67-99.ngrok-free.app'
+ALLOWED_HOSTS = ['noteslidebackend.onrender.com']
 
 
 # Application definition
