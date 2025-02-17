d25ccce35f6f4b1d5f7aca8796addce9183b0815
Aditya Byju <byjuaditya@gmail.com>
2025-02-17 11:10:24 -0600

    Fix sitemap to use correct domain (note-slide.com)

--- api/sitemap.py
+++ api/sitemap.py
@@ -9,13 +9,24 @@ db = client["NoteSlide"]
 notes_collection = db["Notes"]
 
 class NoteSitemap(Sitemap):
-    domain = 'note-slide.com'  # Need to specify the domain
+    protocol = 'https'
+
+    def get_urls(self, page=1, site=None, protocol=None):
+        # Override get_urls to prevent Django from prepending the current site domain
+        urls = []
+        for item in self.items():
+            urls.append({
+                'location': self.location(item),
+                'lastmod': None,
+                'changefreq': None,
+                'priority': None
+            })
+        return urls
 
     def items(self):
         urls = list(notes_collection.find({}, {"_id": 1}))
         return urls
 
     def location(self, item):
-        return f"/view/{item['_id']}/"
-
+        return f"https://note-slide.com/view/{item['_id']}/"
     
